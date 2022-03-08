595598f691f190c75b52f27dbce1d289b94dfc38
dyllanwli <messsking@gmail.com>
2022-03-08 00:04:18 +0000

    initial update

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 pip install git+https://github.com/zksync-sdk/zksync-python.git
+pip install eth-tester
+
+wget https://github.com/zksync-sdk/zksync-crypto-c/releases/download/v0.1.2/zks-crypto-linux-x64.a
+wget https://github.com/zksync-sdk/zksync-crypto-c/releases/download/v0.1.2/zks-crypto-linux-x64.d
+wget https://github.com/zksync-sdk/zksync-crypto-c/releases/download/v0.1.2/zks-crypto-linux-x64.so
+
+export ZK_SYNC_LIBRARY_PATH=$PWD/lib
+echo $ZK_SYNC_LIBRARY_PATH
